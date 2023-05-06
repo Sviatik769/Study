f643aa0bc6a01c2b4950f4e9f23e21eacfc6390a
Sviatik769 <msvatoslav769@gmail.com>
2023-05-06 12:06:45 +0300

    sims

--- main.py
+++ main.py
@@ -11,6 +11,96 @@ class Human:
         self.car = car
         self.home = home
 
+    def get_house(self):
+        self.home = House()
+
+    def get_car(self):
+        self.car = Auto(brand_of_car)
+
+    def get_job(self):
+        if self.car.drive():
+            pass
+        else:
+            self.to_repair()
+            return
+        self.job = Job(job_list)
+
+    def eat(self):
+        if self.home.food <= 0:
+            self.shoping('food')
+        else:
+            if self.satiety >= 100
+                self.satiety = 100
+                return
+            self.satiety += 5
+            self.home.food -= 5
+
+    def work(self):
+        if self.car.drive():
+            pass
+        else:
+            if self.car.fuel < 20
+                self.shoping('fuel')
+                return
+            self.money += self.job.salary
+            self.gladness -= self.job.gladness_less
+            self.satiety -= 4
+
+    def shoping(self, manage):
+        #      --------------Here-----------------
+        if self.car.drive():
+            pass
+        else:
+            if self.car.fuel < 20:
+                manage = 'fuel'
+            else:
+                self.to_repair()
+                return
+        if manage == 'fuel':
+            print('I bought fuel')
+            self.money -= 100
+            self.car.fuel += 100
+        elif manage == 'food':
+            print('I bought food')
+            self.money -= 50
+            self.home.food += 50
+        elif manage == 'delicacies':
+            print('I bought delicacies')
+            self.money -= 15
+            self.gladness += 10
+            self.satiety += 2
+
+    def chill(self):
+        self.gladness += 10
+        self.home.mess += 5
+
+    def clean_home(self):
+        self.gladness -= 5
+        self.home.mess = 0
+
+    def to_repair(self):
+        self.car.strangth += 100
+        self.money -= 50
+
+    def days_indexes(self, day):
+        day = f" Today the {day} of {self.name}'s life"
+        print(f"{day:=^50}", "\n")
+        human_indexes = self.name + "'s indexes"
+        print(f"{human_indexes:^50}", "\n")
+        print(f"Money - {self.money}")
+        print(f"Satiety - {self.satiety}")
+        print(f"Gladness - {self.gladness}")
+        home_indexes = "Home indexes"
+        print(f"{home_indexes:^50}", "\n")
+        print(f"Food - {self.home.food}")
+        print(f"Mess - {self.home.mess}")
+        car_indexes = f"{self.car.brand} car indexes"
+        print(f"{car_indexes:^50}", "\n")
+        print(f"fuel - {self.car.fuel}")
+        print(f"strangth - {self.car.strangth}")
+
+
+#      --------------Here-----------------
 
 
 class Auto:
@@ -42,9 +132,16 @@ job_list = {
     'C++ developer': {'salary': 45, 'gladness_less': 25};
     'Rust developer': {'salary': 70, 'gladness_less': 1};
 }
-brand_of_car = {
-    'BMW': {'fuel': 100, 'strangth': 100, 'consuption': 6};
-    'Lada': {'fuel': 50, 'strangth': 40, 'consuption': 10};
-    'Volvo': {'fuel': 70, 'strangth': 150, 'consuption': 8};
-    'BMW': {'fuel': 100, 'strangth': 100, 'consuption': 6};
+brands_of_car = {
+    'BMW': {'fuel': 100, 'strength': 100, 'consumption': 6},
+    'Lada': {'fuel': 50, 'strength': 40, 'consumption': 10},
+    'Volvo': {'fuel': 70, 'strength': 150, 'consumption': 8},
+    'Ferrari': {'fuel': 80, 'strength': 120, 'consumption': 14},
 }
+
+
+class Job:
+    def __init__(self, job_list):
+        self.job = random.choice(list(job_list))
+        self.salary = job_list[self.job]['salary']
+        self.gladness_less = job_list[self.job]['gladness_less']
