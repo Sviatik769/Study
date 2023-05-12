d83ef83eca1a3875d2cde3bbe0de285fb4a6b409
Sviatik769 <msvatoslav769@gmail.com>
2023-05-12 18:56:10 +0300

    turtle

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-
+import turtle
+window = turtle.Screen()
+turtle.shape('turtle')
+turtle.bgcolor('black')
+turtle.color('white')
+turtle.pensize(2)
+turtle.speed(100000)
+for i in range(360):
+    turtle.pensize(i/100 + 1)
+    turtle.forward(i)
+    turtle.left(i/45 + 100)
+    turtle.forward(i)
+window.exitonclick()
