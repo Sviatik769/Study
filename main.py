3a9930d9b452c5f58d858b1e414a22c654911443
Sviatik769 <msvatoslav769@gmail.com>
2023-11-11 11:14:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,44 @@
+#Задача1
 
-class Computer:
-    def __init__(self):
-        self.monitor = 5700
-        self.corpus = 1800
-        self.mouse = 780
-        self.claviatura = 1890
+inches = 1
+centimeter = 2.54
 
-calculation()
+print((inches * centimeter),("centimeters"))
+
+#Задача2
+
+kilogram = 1
+milligrams = 1000000
+
+print(" ")
+print((kilogram * milligrams),("milligrams"))
+
+#Задача3
+
+meters = 1000
+kilometers = 1000
+
+print(" ")
+print((meters / kilometers),("kilometers"))
+
+#Задача4
+
+kilometers = 1000
+miles = 0.621371
+
+print(" ")
+print((kilometers * miles),("miles"))
+
+#Задача5
+
+print(" ")
+print("New list 1.Ivan 2.Sviatoslav 3.Sofia 4.Denis 5.Bogdan")
+print(" ")
+print("New list 1.Ivan 2.Sviatoslav 3.Sofia 4.Denis 5.Bogdan 6.Petro")
+print(" ")
+print("New list 1.Ivan 2.Sviatoslav 4.Denis 5.Bogdan 6.Petro")
+
+
+
+
+
