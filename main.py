e5b57d2be03e821ef4a0fba90dca6ea03d31bf87
Sviatik769 <msvatoslav769@gmail.com>
2023-11-18 10:40:38 +0200

    exercise 1
    exercise 1

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+print('Hello, what`s your name')
 
+name = input()
+
+print('Hello', name)
+
+Name = ('Sviatoslav')
+Surname = ('Maistrenko')
+
+print(Name, Surname)
+
+print('How old are you')
+
+Years = int(input())
+
+if Years >= 18:
+    print('All good')
+else:
+    print('You can`t visit this site')
+
+for i in range(2, 21, 2)
