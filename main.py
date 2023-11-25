90cbcf8bc9455ae1cb3479f9efd4c167a2d1fa4c
Sviatik769 <msvatoslav769@gmail.com>
2023-11-25 09:43:44 +0200

    exersice-3

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
+#Задача 1
 
+year = int(input("Скільки вам років:"))
+
+if year < 18:
+    print("Ви ще неповнолітній")
+else:
+    print("Ви вже повнолітній")
+print(" ")
+#Задача 2
+
+list = [1, 56 ,32, 81, 26, 69, 15, 8, 74, 4]
+
+max_number = max(list)
+print("Найбільше число", max_number)
+print(" ")
+#Задача 3
+
+number = int(input("Ведіть множник:"))
+
+range i in(1, 10)
+
+print(number * i)
